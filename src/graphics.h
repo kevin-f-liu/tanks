63b8a1f10ad59a141b0f8f2412e0b82440999481
Allison Chan <allisonchanykei@gmail.com>
2018-11-13 16:08:49 -0500

    added uart.h

--- src/graphics.h
+++ src/graphics.h
@@ -1,21 +1,21 @@
 /*
-* graphics.h
-* Graphics helper functions header 
-*/
+ * graphics.h
+ * Graphics helper functions header
+ */
 
+#include "uart.h"
 #ifndef _GRAPHICS_H
 #define _GRAPHICS_H
 
-#define TERRAIN_WIDTH 320 // Screen width in pixels
-#define TERRAIN_HEIGHT 240 // Screen width in pixels
+#define TERRAIN_WIDTH 320   // Screen width in pixels
+#define TERRAIN_HEIGHT 240  // Screen width in pixels
 
 typedef char Terrain;
 
 // Global terrain array
 extern Terrain terrain;
 
-
-extern void graphicsWorker(void const *arg);
+extern void graphicsWorker(void const* arg);
 extern void initGraphics(uint16_t cColor, uint16_t bColor, uint16_t tColor);
 void displayStringToLCD(int row, int column, int sz, char* str, int clear);
 
