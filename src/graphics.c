6e73bc4cabd715a04bcc162a7695406be4c11f3c
Allison Chan <allisonchanykei@gmail.com>
2018-11-20 15:38:03 -0500

    fixed formatting

--- src/graphics.c
+++ src/graphics.c
@@ -1,16 +1,16 @@
 #include "graphics.h"
-#include <cmsis_os.h>
-#include <lpc17xx.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "GLCD.h"
 #include "coordinate.h"
 #include "helper.h"
 #include "player.h"
 #include "sprites.h"
 #include "terrain.h"
+#include <cmsis_os.h>
+#include <lpc17xx.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Data for all sprites
 typedef struct {
@@ -18,7 +18,7 @@ typedef struct {
   int py;
   char width;
   char height;
-  const uint16_t *spritemap;  // Location of spritemap, which is constant
+  const uint16_t *spritemap; // Location of spritemap, which is constant
 } base_sprite;
 
 // Tank sprite
@@ -43,7 +43,7 @@ typedef struct {
 
 // Data to track health and power bars
 typedef struct {
-  int8_t val;  // out of 100
+  int8_t val; // out of 100
   uint16_t x;
   uint16_t y;
   uint16_t width;
@@ -76,14 +76,14 @@ void resetVars() {
   t1->base.width = TANK_WIDTH;
   t1->base.height = TANK_HEIGHT;
   t1->barrelOffset = TANK_WIDTH - TANK_HEIGHT;
-  t1->pc->x = 0;  // Kinda wrong, but overwritten very quickly
+  t1->pc->x = 0; // Kinda wrong, but overwritten very quickly
   t1->pc->y = 0;
   t2->base.px = 0;
   t2->base.py = TANK_WIDTH - TANK_HEIGHT;
   t2->base.width = TANK_WIDTH;
   t2->base.height = TANK_HEIGHT;
   t2->barrelOffset = TANK_WIDTH - TANK_HEIGHT;
-  t2->pc->x = 0;  // Kinda wrong, but overwritten very quickly
+  t2->pc->x = 0; // Kinda wrong, but overwritten very quickly
   t2->pc->y = 0;
   // Init barrels
   b1->base.px = 0;
@@ -107,7 +107,7 @@ void resetVars() {
   p2_hp->x = TERRAIN_WIDTH_PX - VALUE_BAR_WIDTH;
   p1_hp->y = 10;
   p2_hp->y = 10;
-  power->x = TERRAIN_WIDTH_PX / 2 - VALUE_BAR_WIDTH / 2;  // Centered
+  power->x = TERRAIN_WIDTH_PX / 2 - VALUE_BAR_WIDTH / 2; // Centered
   power->y = 10;
   p1_hp->height = VALUE_BAR_HEIGHT;
   p2_hp->height = VALUE_BAR_HEIGHT;
@@ -120,7 +120,7 @@ void resetVars() {
   power->color = POWER_COLOR;
   p1_hp->val = MAX_BAR_VAL + 1; // Just need a value highter than max value
   p2_hp->val = MAX_BAR_VAL + 1;
-  power->val = MAX_BAR_VAL + 1; 
+  power->val = MAX_BAR_VAL + 1;
 }
 void initGraphics(uint16_t cColor, uint16_t bColor, uint16_t tColor, Terrain *te) {
   t = te; // Set global
@@ -149,12 +149,13 @@ void displayStringToLCD(int row, int column, int sz, char *str, int clear) {
   // Display a string to a position on the LCD, clears the amount of characters
   // needed.
   if (clear) {
-    char *blanks = (char *)malloc(clear + 1);  // Allocate string of size clear
+    char *blanks = (char *)malloc(clear + 1); // Allocate string of size clear
     // Fill with spaces and null termination char
-    for (int i = 0; i < clear; i++) blanks[i] = ' ';
+    for (int i = 0; i < clear; i++)
+      blanks[i] = ' ';
     blanks[clear] = '\0';
-    GLCD_DisplayString(row, column, sz,
-                       blanks);  // clear existing text, exactly as many needed
+    // clear existing text, exactly as many needed
+    GLCD_DisplayString(row, column, sz, blanks);
     free(blanks);
     blanks = NULL;
   }
@@ -162,24 +163,24 @@ void displayStringToLCD(int row, int column, int sz, char *str, int clear) {
   GLCD_DisplayString(row, column, sz, str);
 }
 
-uint16_t pixelFromCoord(uint16_t val) { return val * PX_PER_BLOCK; }
+uint16_t pixelFromCoord(uint16_t val) {
+  return val * PX_PER_BLOCK;
+}
 
-void displayBitmapToLCD(int col, int row, int width, int height,
-                        const uint16_t *bitmap) {
+void displayBitmapToLCD(int col, int row, int width, int height, const uint16_t *bitmap) {
   // Simple wrapper
-  GLCD_Bitmap(col, row, width, height,
-              (unsigned char *)bitmap);  // Allow larger bitmaps in
+  // Allow larger bitmaps in
+  GLCD_Bitmap(col, row, width, height, (unsigned char *)bitmap);
 }
 
 void displayBlock(int x, int y, const uint16_t *bitmap) {
   // Give block as terrain coordinate
-  displayBitmapToLCD(pixelFromCoord(x), pixelFromCoord(y), PX_PER_BLOCK,
-                     PX_PER_BLOCK, bitmap);
+  displayBitmapToLCD(pixelFromCoord(x), pixelFromCoord(y), PX_PER_BLOCK, PX_PER_BLOCK, bitmap);
 }
 
-void drawRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height,
-              uint16_t color) {
-  if (!width || !height) return;
+void drawRect(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint16_t color) {
+  if (!width || !height)
+    return;
   // draw a rectangle of pixels really fast
   int i, j;
 
@@ -208,7 +209,8 @@ void clearBlock(int x, int y) {
 const uint16_t *getTerrainMap(Terrain *t, Coordinate c) {
   // Get the classification of a terrain coordinate, as in peak/slope etc and
   // return a pointer to its map
-  if (isEdge(c, TERRAIN_WIDTH, TERRAIN_HEIGHT)) return terrainFull;
+  if (isEdge(c, TERRAIN_WIDTH, TERRAIN_HEIGHT))
+    return terrainFull;
   if (t->x[getIndex(c.x, c.y + 1)] && !t->x[getIndex(c.x, c.y - 1)]) {
     // Bottom filled
     if (t->x[getIndex(c.x + 1, c.y)] && !t->x[getIndex(c.x - 1, c.y)]) {
@@ -221,8 +223,7 @@ const uint16_t *getTerrainMap(Terrain *t, Coordinate c) {
   return terrainFull;
 }
 
-void drawTerrainSection(Terrain *t, Coordinate c, uint16_t width,
-                        uint16_t height) {
+void drawTerrainSection(Terrain *t, Coordinate c, uint16_t width, uint16_t height) {
   // Render terrain for a specific section
   for (int i = 0; i < height; i++) {
     for (int j = 0; j < width; j++) {
@@ -242,17 +243,18 @@ void drawTerrain(Terrain *t) {
 }
 
 void updateTank(int newX, int newY, int newAng, char player, bool redraw) {
-  // x and y are the top left corner of a widthXwidth square. Same as barrel  
+  // x and y are the top left corner of a widthXwidth square. Same as barrel
   tank_graph_t *tank = player == 1 ? t1 : t2;
   barrel_graph_t *barrel = player == 1 ? b1 : b2;
   barrel_graph_t *otherBarrel = player == 1 ? b2 : b1;
-  
+
   // Don't do anything if nothing changes
-  if (!redraw && newX == barrel->base.px && newY == barrel->base.py && newAng == barrel->angle) return;
+  if (!redraw && newX == barrel->base.px && newY == barrel->base.py && newAng == barrel->angle)
+    return;
 
   // Update barrel first, if invalid angle, then don't change it
   // If the barrel was previously drawn on the same tank, don't redraw if not changed
-  if ((!barrel->alreadyDrawn || barrel->angle != newAng)  && newAng <= 180 && newAng >= -180) {
+  if ((!barrel->alreadyDrawn || barrel->angle != newAng) && newAng <= 180 && newAng >= -180) {
     barrel->alreadyDrawn = true;
     otherBarrel->alreadyDrawn = false;
     loadBarrelmap(newAng);
@@ -260,15 +262,12 @@ void updateTank(int newX, int newY, int newAng, char player, bool redraw) {
 
   if (!redraw) {
     // Barrel map completely overlaps tank bitmap, so clear that one
-    clearRect(barrel->base.px, barrel->base.py, barrel->base.width,
-            barrel->base.height);
+    clearRect(barrel->base.px, barrel->base.py, barrel->base.width, barrel->base.height);
   }
 
   // Barrel update should go first to draw tank on top of barrel
-  displayBitmapToLCD(newX, newY, barrel->base.width, barrel->base.height,
-                     barrel->base.spritemap);
-  displayBitmapToLCD(newX, newY + tank->barrelOffset, tank->base.width,
-                     tank->base.height, tank->base.spritemap);
+  displayBitmapToLCD(newX, newY, barrel->base.width, barrel->base.height, barrel->base.spritemap);
+  displayBitmapToLCD(newX, newY + tank->barrelOffset, tank->base.width, tank->base.height, tank->base.spritemap);
 
   barrel->base.px = newX;
   barrel->base.py = newY;
@@ -317,11 +316,11 @@ void initTank(Coordinate c, int angle, int player) {
 void updateShot(int newX, int newY) {
   // Update to pixel coordinate
   // Don't do anything if nothing changes
-  if (newX == shot->base.px && newY == shot->base.py) return;
-  
+  if (newX == shot->base.px && newY == shot->base.py)
+    return;
+
   clearRect(shot->base.px, shot->base.py, shot->base.width, shot->base.height);
-  displayBitmapToLCD(newX, newY, shot->base.width, shot->base.height,
-                     shot->base.spritemap);
+  displayBitmapToLCD(newX, newY, shot->base.width, shot->base.height, shot->base.spritemap);
   shot->base.px = newX;
   shot->base.py = newY;
 }
@@ -360,7 +359,7 @@ void animateExplosion(Coordinate c, Terrain *t) {
       count--;
     }
   }
-  
+
   // Redraw tanks
   updateTank(b1->base.px, b1->base.py, b1->angle, 1, true);
   updateTank(b2->base.px, b2->base.py, b2->angle, 2, true);
@@ -381,9 +380,7 @@ void updateBar(value_bar_t *b, uint8_t newVal) {
   if (b->val > MAX_BAR_VAL) {
     // Init condition for a new bar
     drawRect(b->x, b->y, barValueToPixels(newVal), b->height, b->color);
-    drawRect(b->x + barValueToPixels(newVal), b->y,
-             barValueToPixels(MAX_BAR_VAL - newVal), b->height,
-             EMPTY_BAR_COLOR);
+    drawRect(b->x + barValueToPixels(newVal), b->y, barValueToPixels(MAX_BAR_VAL - newVal), b->height, EMPTY_BAR_COLOR);
     b->val = newVal;
     return;
   }
@@ -412,7 +409,9 @@ void updateHealthBar(uint8_t newVal, int player) {
   updateBar(bar, newVal);
 }
 
-void updatePowerBar(uint8_t newVal) { updateBar(power, newVal); }
+void updatePowerBar(uint8_t newVal) {
+  updateBar(power, newVal);
+}
 
 void drawPermText(void) {
   displayStringToLCD(0, 3, 0, "Player 1", 0);
